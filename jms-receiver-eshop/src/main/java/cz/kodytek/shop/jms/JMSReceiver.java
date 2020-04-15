bfacea3783cd0681e32aabdacb90816105330e00
Samuel Kodytek <samuel@kodytek.cz>
2020-04-15 23:26:03 +0200

    Fix

--- jms-receiver-eshop/src/main/java/cz/kodytek/shop/jms/JMSReceiver.java
+++ jms-receiver-eshop/src/main/java/cz/kodytek/shop/jms/JMSReceiver.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-public class JMSSender {
+public class JMSReceiver {
 
     public static void main(String[] args) {
         Context namingContext = null;
